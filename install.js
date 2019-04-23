509d8e595e694b7688b71f877a4792ff2c9abdc4
Karen <kaskar2008@mail.ru>
2019-04-24 02:01:47 +0300

    Update install - rimraf to async

--- install.js
+++ install.js
@@ -13,22 +13,23 @@ const simpleGit = require('simple-git')(tmpDirPath);
 simpleGit.init(() => {
   simpleGit.addRemote('origin', 'https://github.com/ke-mantha/mantha-template-default.git', () => {
     simpleGit.pull('origin', 'master', {}, () => {
-      console.log(`removing .git dir at ${path.join(tmpDirPath, '.git')} ...`);
-      rimraf.sync(path.join(tmpDirPath, '.git'));
-      console.log('ok\r');
-      console.log('copying files...');
-      ncp(path.join(tmpDirPath, '.'), process.env.PWD, function (err) {
-        if (err) {
-          return console.error(err);
-        }
+      console.log(`removing .git...`);
+      rimraf(path.join(tmpDirPath, '.git'), () => {
         console.log('ok\r');
-        console.log(`removing temp dir at ${tmpDirPath} ...`);
-        rimraf(tmpDirPath, () => {
+        console.log('copying files...');
+        ncp(path.join(tmpDirPath, '.'), process.env.PWD, function (err) {
+          if (err) {
+            return console.error(err);
+          }
           console.log('ok\r');
-          console.log('installing environment...');
-          rimraf.sync(path.join(process.env.PWD, 'package-lock.json'));
-          const exec = require('child_process').exec;
-          child = exec(`cd ${process.env.PWD} && npm i`).stderr.pipe(process.stderr);
+          console.log(`removing temp dir at ${tmpDirPath} ...`);
+          rimraf(tmpDirPath, () => {
+            console.log('ok\r');
+            console.log('installing environment...');
+            rimraf.sync(path.join(process.env.PWD, 'package-lock.json'));
+            const exec = require('child_process').exec;
+            child = exec(`cd ${process.env.PWD} && npm i`).stderr.pipe(process.stderr);
+          });
         });
       });
     })
