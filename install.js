8cc34fe0d742f0122b3a687af7ef48065022abdc
Karen <kaskar2008@mail.ru>
2019-04-23 20:25:44 +0300

    Update install script - add simple git

--- install.js
+++ install.js
@@ -1,1 +1,11 @@
-console.log(process.env);
+const fs = require('fs');
+const path = require('path');
+const simpleGit = require('simple-git');
+
+simpleGit.init(() => {
+  simpleGit.addRemote('origin', 'https://github.com/ke-mantha/mantha-template-default.git', () => {
+    simpleGit.pull('origin', 'master', () => {
+      fs.rmdirSync(path.join(process.env.PWD, '.git'));
+    })
+  })
+});
