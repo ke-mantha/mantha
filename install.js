1f2ca41bb51f33dbfc242fb4ed938293f583d594
Karen <kaskar2008@mail.ru>
2019-04-23 21:02:56 +0300

    Update install - use rimraf

--- install.js
+++ install.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const rimraf = require('rimraf');
 
-const tmpDirName = '$_tmp-dir';
+const tmpDirName = '.tmpdir';
 const tmpDirPath = path.join(process.env.PWD, tmpDirName);
 
 fs.mkdirSync(tmpDirPath);
@@ -11,14 +12,14 @@ const simpleGit = require('simple-git')(tmpDirPath);
 simpleGit.init(() => {
   simpleGit.addRemote('origin', 'https://github.com/ke-mantha/mantha-template-default.git', () => {
     simpleGit.pull('origin', 'master', {}, () => {
-      console.log(`rmdirSync ${path.join(tmpDirPath, '.git')} ...`);
-      fs.rmdirSync(path.join(tmpDirPath, '.git'));
+      console.log(`rimraf ${path.join(tmpDirPath, '.git')} ...`);
+      rimraf.sync(path.join(tmpDirPath, '.git'));
       console.log('ok');
       console.log('copyFileSync...');
       fs.copyFileSync(path.join(tmpDirPath, '*'), process.env.PWD);
       console.log('ok');
-      console.log(`rmdirSync ${tmpDirPath} ...`);
-      fs.rmdirSync(tmpDirPath);
+      console.log(`rimraf ${tmpDirPath} ...`);
+      rimraf.sync(tmpDirPath);
       console.log('ok');
     })
   })
