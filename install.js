ebdada497b5a4b83cc1a7627e0dc29fa0d8d966d
Karen <kaskar2008@mail.ru>
2019-04-23 20:51:28 +0300

    Update install - add mkdirSync

--- install.js
+++ install.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const simpleGit = require('simple-git')(path.join(process.env.PWD, '$_tmp-dir'));
+
+const tmpDirName = '$_tmp-dir';
+const tmpDirPath = path.join(process.env.PWD, tmpDirName);
+
+fs.mkdirSync(tmpDirPath);
+
+const simpleGit = require('simple-git')(tmpDirPath);
 
 simpleGit.init(() => {
   simpleGit.addRemote('origin', 'https://github.com/ke-mantha/mantha-template-default.git', () => {
     simpleGit.pull('origin', 'master', () => {
-      fs.rmdirSync(path.join(process.env.PWD, '$_tmp-dir', '.git'));
-      fs.copyFileSync(path.join(process.env.PWD, '$_tmp-dir'), process.env.PWD);
-      fs.rmdirSync(path.join(process.env.PWD, '$_tmp-dir'));
+      fs.rmdirSync(path.join(tmpDirPath, '.git'));
+      fs.copyFileSync(tmpDirPath, process.env.PWD);
+      fs.rmdirSync(tmpDirPath);
     })
   })
 });
