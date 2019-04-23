0f85065a37700dcf17855fd769b934b961d5d367
Karen <kaskar2008@mail.ru>
2019-04-23 20:46:57 +0300

    Update install - tmp dir middlware

--- install.js
+++ install.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const simpleGit = require('simple-git');
+const simpleGit = require('simple-git')(path.join(process.env.PWD, '$_tmp-dir'));
 
 simpleGit.init(() => {
   simpleGit.addRemote('origin', 'https://github.com/ke-mantha/mantha-template-default.git', () => {
     simpleGit.pull('origin', 'master', () => {
-      fs.rmdirSync(path.join(process.env.PWD, '.git'));
+      fs.rmdirSync(path.join(process.env.PWD, '$_tmp-dir', '.git'));
+      fs.copyFileSync(path.join(process.env.PWD, '$_tmp-dir'), process.env.PWD);
+      fs.rmdirSync(path.join(process.env.PWD, '$_tmp-dir'));
     })
   })
 });
