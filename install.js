21f09fdddb0dc0c811278e64239f9b4b29508903
Karen <dev.kaskar2008@gmail.com>
2019-04-24 14:59:38 +0300

    Update install - logs and project directory change

--- install.js
+++ install.js
@@ -3,37 +3,40 @@ const path = require('path');
 const rimraf = require('rimraf');
 const ncp = require('ncp').ncp;
 
-console.log('PWD FTW', process.env.PWD);
-console.log('HOME FTW', process.env.HOMEPATH);
-console.log('env FTW', process.env);
-
+const projectDir = path.join(process.cwd(), '../../');
 const tmpDirName = '.tmpdir';
-const tmpDirPath = path.join(process.env.PWD, tmpDirName);
+const tmpDirPath = path.join(projectDir, tmpDirName);
 
 fs.mkdirSync(tmpDirPath);
 
 const simpleGit = require('simple-git')(tmpDirPath);
 
 simpleGit.init(() => {
+  console.log(`installing template...`);
+  console.log('ok');
+  console.log('');
   simpleGit.addRemote('origin', 'https://github.com/ke-mantha/mantha-template-default.git', () => {
     simpleGit.pull('origin', 'master', {}, () => {
       console.log(`removing .git...`);
       rimraf(path.join(tmpDirPath, '.git'), () => {
-        console.log('ok\r\r');
+        console.log('ok');
+        console.log('');
         console.log('copying files...');
-        ncp(path.join(tmpDirPath, '.'), process.env.PWD, function (err) {
+        ncp(path.join(tmpDirPath, '.'), projectDir, function (err) {
           if (err) {
             return console.error(err);
           }
-          console.log('ok\r\r');
+          console.log('ok');
+          console.log('');
           console.log(`removing temp dir at ${tmpDirPath} ...`);
           rimraf(tmpDirPath, () => {
-            console.log('ok\r\r');
-            rimraf(path.join(process.env.PWD, 'package-lock.json'), () => {
-              rimraf(path.join(process.env.PWD, 'node_modules'), () => {
+            console.log('ok');
+            console.log('');
+            rimraf(path.join(projectDir, 'package-lock.json'), () => {
+              rimraf(path.join(projectDir, 'node_modules'), () => {
                 console.log('installing environment...');
                 var child_process = require('child_process');
-                child_process.execSync(`cd ${process.env.PWD} && npm i`, {stdio:[0,1,2]});  
+                child_process.execSync(`cd ${projectDir} && npm i`, {stdio:[0,1,2]});  
               });
             });
           });
